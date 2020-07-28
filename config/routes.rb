f88e910fb77cd7d15d3f6f30f9dbc16182b02a05
tony <liangyao910@gmail.com>
2020-07-28 21:44:12 +0900

    index importのcontrollerとmodel実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'csv/import'
+  post 'csv/import'
+  get 'csv/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
